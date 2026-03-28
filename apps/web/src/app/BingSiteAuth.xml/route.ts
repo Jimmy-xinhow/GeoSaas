011f4ccaffd645e30085980d10e4f20bd248e4d4
Jimmy <jimmy@xinhow.com.tw>
2026-03-28 22:22:17 +0800

    Add Bing Webmaster Tools verification file

---
+++ apps/web/src/app/BingSiteAuth.xml/route.ts
@@ -0,0 +1,9 @@
+export async function GET() {
+  const xml = `<?xml version="1.0"?>
+<users>
+	<user>69F7520D35AA9839E35AFCEB0FF93397</user>
+</users>`;
+  return new Response(xml, {
+    headers: { 'Content-Type': 'application/xml' },
+  });
+}
